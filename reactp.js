86ed052dbdd512eccd295cecc6b7313a930453da
Nayeem-seu45 <2017000000034@seu.edu.bd>
2021-09-01 18:20:32 +0600

    setup react-app

--- reactp.js
+++ reactp.js
@@ -1,1 +1,22 @@
-ssxxd
+//Components
+
+
+//function Component
+export const Header =()=>{
+    return(
+        <div>
+            <h1>my Header</h1>
+        </div>
+    )
+}
+
+//Class Component
+
+export default class header extends React.Component{
+    render(){
+        <div>
+            <h1>my Header</h1>
+        </div>
+    }
+}
+
